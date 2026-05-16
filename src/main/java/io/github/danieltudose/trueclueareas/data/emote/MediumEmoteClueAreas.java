f4fe1f558fc9c9bf191d555174f538c0fe086e1c
Daniel-Tudose <tudosedaniel24@stud.ase.ro>
2026-05-16 18:24:07 +0300

    add area for Beckon in the Shayzien Combat Ring step

--- src/main/java/io/github/danieltudose/trueclueareas/data/emote/MediumEmoteClueAreas.java
+++ src/main/java/io/github/danieltudose/trueclueareas/data/emote/MediumEmoteClueAreas.java
@@ -10,5 +10,7 @@ public class MediumEmoteClueAreas {
     public static final Map<String, DigArea> AREAS = new ImmutableMap.Builder<String, DigArea>()
             .put("Jump for Joy in the mine near the Twilight Temple. Bow before you talk to me. Equip a maple longbow, a ruby amulet and some steel platelegs.",
                     new DigArea(new WorldPoint(1668, 3280, 0), new WorldPoint(1676, 3286,0)))
+            .put("Beckon in the Shayzien Combat Ring. Show your anger before you talk to me. Equip an adamant platebody, adamant full helm and adamant platelegs.",
+                    new DigArea(new WorldPoint(1539, 3619, 0), new WorldPoint(1548, 3627,0)))
             .build();
 }
