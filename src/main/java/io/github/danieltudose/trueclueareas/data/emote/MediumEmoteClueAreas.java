283341522699f2792b498e71b3ffda45fea30582
Daniel-Tudose <tudosedaniel24@stud.ase.ro>
2026-05-16 18:13:46 +0300

    added the are for Jump for Joy in the mine near the Twilight Temple step

--- src/main/java/io/github/danieltudose/trueclueareas/data/emote/MediumEmoteClueAreas.java
+++ src/main/java/io/github/danieltudose/trueclueareas/data/emote/MediumEmoteClueAreas.java
@@ -2,11 +2,13 @@ package io.github.danieltudose.trueclueareas.data.emote;
 
 import io.github.danieltudose.trueclueareas.data.DigArea;
 import com.google.common.collect.ImmutableMap;
+import net.runelite.api.coords.WorldPoint;
 
 import java.util.Map;
 
 public class MediumEmoteClueAreas {
     public static final Map<String, DigArea> AREAS = new ImmutableMap.Builder<String, DigArea>()
-            // No entries yet — contribute yours!
+            .put("Jump for Joy in the mine near the Twilight Temple. Bow before you talk to me. Equip a maple longbow, a ruby amulet and some steel platelegs.",
+                    new DigArea(new WorldPoint(1668, 3280, 0), new WorldPoint(1676, 3286,0)))
             .build();
 }
