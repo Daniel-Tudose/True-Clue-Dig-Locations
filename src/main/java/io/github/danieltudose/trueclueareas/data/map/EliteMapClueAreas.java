0c54f8ba2a668d8a56a3372a16d9ffc21ee9213b
Daniel-Tudose <tudosedaniel24@stud.ase.ro>
2026-05-19 06:47:35 +0300

    Add new Elite Map clue step entry

--- src/main/java/io/github/danieltudose/trueclueareas/data/map/EliteMapClueAreas.java
+++ src/main/java/io/github/danieltudose/trueclueareas/data/map/EliteMapClueAreas.java
@@ -2,11 +2,14 @@ package io.github.danieltudose.trueclueareas.data.map;
 
 import io.github.danieltudose.trueclueareas.data.DigArea;
 import com.google.common.collect.ImmutableMap;
+import net.runelite.api.coords.WorldPoint;
+import net.runelite.api.gameval.ItemID;
 
 import java.util.Map;
 
 public class EliteMapClueAreas {
     public static final Map<Integer, DigArea> AREAS = new ImmutableMap.Builder<Integer, DigArea>()
-            // No entries yet — contribute yours!
+            .put(ItemID.TRAIL_ELITE_MAP_EXP1, // North east of Castle Wars, just west of fairy ring CIQ
+                    new DigArea(new WorldPoint(2447, 3108, 0), new WorldPoint(2455, 3116, 0)))
             .build();
 }
