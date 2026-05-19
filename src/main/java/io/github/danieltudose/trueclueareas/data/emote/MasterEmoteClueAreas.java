ca5cdb0c9fb093d4cd8565a74918e1311a4e8d17
Daniel-Tudose <tudosedaniel24@stud.ase.ro>
2026-05-19 07:00:49 +0300

    Add 2 Master Emote clue steps entries

--- src/main/java/io/github/danieltudose/trueclueareas/data/emote/MasterEmoteClueAreas.java
+++ src/main/java/io/github/danieltudose/trueclueareas/data/emote/MasterEmoteClueAreas.java
@@ -2,11 +2,15 @@ package io.github.danieltudose.trueclueareas.data.emote;
 
 import io.github.danieltudose.trueclueareas.data.DigArea;
 import com.google.common.collect.ImmutableMap;
+import net.runelite.api.coords.WorldPoint;
 
 import java.util.Map;
 
 public class MasterEmoteClueAreas {
     public static final Map<String, DigArea> AREAS = new ImmutableMap.Builder<String, DigArea>()
-            // No entries yet — contribute yours!
+            .put("Cry in the TzHaar gem store. Beware of double agents! Equip a fire cape and TokTz-Xil-Ul.",
+                    new DigArea(new WorldPoint(2466 , 5147 , 0), new WorldPoint(2463, 5150,0)))
+            .put("Think on the western coast of Salvager Overlook. Beware of double agents! Equip a Hueycoatl hide coif and some Hueycoatl hide vambraces.",
+                    new DigArea(new WorldPoint(1613, 3296 , 0), new WorldPoint(1610, 3305 ,0)))
             .build();
 }
