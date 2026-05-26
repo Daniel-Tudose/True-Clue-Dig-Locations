015736a57edf6a451fee4304039faad72682bea9
Daniel-Tudose <tudosedaniel24@stud.ase.ro>
2026-05-26 20:25:51 +0300

    add 21 new entries on easy emote steps

--- src/main/java/io/github/danieltudose/trueclueareas/data/emote/EasyEmoteClueAreas.java
+++ src/main/java/io/github/danieltudose/trueclueareas/data/emote/EasyEmoteClueAreas.java
@@ -2,11 +2,55 @@ package io.github.danieltudose.trueclueareas.data.emote;
 
 import io.github.danieltudose.trueclueareas.data.DigArea;
 import com.google.common.collect.ImmutableMap;
+import net.runelite.api.coords.WorldPoint;
 
 import java.util.Map;
 
 public class EasyEmoteClueAreas {
     public static final Map<String, DigArea> AREAS = new ImmutableMap.Builder<String, DigArea>()
-            // No entries yet — contribute yours!
+            .put("Dance a jig behind the bar on the Pandemonium. Equip a right eye patch and a bronze scimitar.",
+                    new DigArea(new WorldPoint(3048 ,2965, 0), new WorldPoint(3052, 2967, 0)))
+            .put("Bow in the office of the Emir's Arena. Equip an iron chain body, leather chaps and coif.",
+                    new DigArea(new WorldPoint(3312, 3240, 0), new WorldPoint(3316, 3244, 0)))
+            .put("Yawn in the Fortis Grand Museum. Equip an emerald necklace, blue skirt, and turqoise gnome robe top.",
+                     new DigArea(new WorldPoint(1710, 3171, 0), new WorldPoint(1714, 3165, 0)))
+            .put("Wave along the south fence of the Lumber Yard. Equip a hard leather body, leather chaps and a bronze axe.",
+                    new DigArea(new WorldPoint(3300, 3488, 0), new WorldPoint(3313, 3492, 0)))
+            .put("Panic on the pier where you catch the Fishing trawler. Have nothing equipped at all when you do.",
+                    new DigArea(new WorldPoint(2676, 3163, 0), new WorldPoint(2676, 3173, 0)))
+            .put("Do a jig in Varrock's rune store. Equip an air tiara and a staff of water.",
+                    new DigArea(new WorldPoint(3252, 3399, 0), new WorldPoint(3254, 3404, 0)))
+            .put("Laugh at the crossroads south of the Sinclair Mansion. Equip a cowl, a blue wizard robe top and an iron scimitar.",
+                    new DigArea(new WorldPoint(2737, 3533, 0), new WorldPoint(2743, 3541, 0)))
+            .put("Cheer at the games room. Have nothing equipped at all when you do.",
+                    new DigArea(new WorldPoint(2194, 4946, 0), new WorldPoint(2221, 4973, 0)))
+            .put("Headbang in the mine north of Al Kharid. Equip a desert shirt, leather gloves and leather boots.",
+                    new DigArea(new WorldPoint(3290, 3274, 0), new WorldPoint(3306, 3317, 0)))
+            .put("Clap in the main exam room in the Exam Centre. Equip a white apron, green gnome boots and leather gloves.",
+                    new DigArea(new WorldPoint(3357, 3332, 0), new WorldPoint(3367, 3348, 0)))
+            .put("Panic in the Limestone Mine. Equip bronze platelegs, a steel pickaxe and a steel medium helmet.",
+                    new DigArea(new WorldPoint(3367 , 3497, 0), new WorldPoint(3375, 3505, 0)))
+            .put("Clap on the top level of the mill, north of East Ardougne. Equip a blue gnome robe top, HAM robe bottom and an unenchanted tiara.",
+                    new DigArea(new WorldPoint(2630, 3383, 2), new WorldPoint(2635, 3388, 2)))
+            .put("Wave on Mudskipper Point. Equip a black cape, leather chaps and a steel mace.",
+                    new DigArea(new WorldPoint(2981, 3106, 0), new WorldPoint(3005, 3130, 0)))
+            .put("Shrug in the mine near Rimmington. Equip a gold necklace, a gold ring and a bronze spear.",
+                    new DigArea(new WorldPoint(2970, 3228, 0), new WorldPoint(2989, 3251, 0)))
+            .put("Spin at the crossroads north of Rimmington. Equip a green gnome hat, cream gnome top and leather chaps.",
+                    new DigArea(new WorldPoint(2979, 3274, 0), new WorldPoint(2984, 3278, 0)))
+            .put("Think in middle of the wheat field by the Lumbridge mill. Equip a blue gnome robetop, a turquoise gnome robe bottom and an oak shortbow.",
+                    new DigArea(new WorldPoint(3155, 3295, 0), new WorldPoint(3160, 3302, 0)))
+            .put("Cheer for the monks at Port Sarim. Equip a coif, steel plateskirt and a sapphire necklace.",
+                    new DigArea(new WorldPoint(3044, 3234, 0), new WorldPoint(3050, 3237, 0)))
+            .put("Jump for joy at the beehives. Equip a desert shirt, green gnome robe bottoms and a steel axe.",
+                    new DigArea(new WorldPoint(2753, 3437, 0), new WorldPoint(2765, 3450, 0)))
+            .put("Dance in the Party Room. Equip a steel full helmet, steel platebody and an iron plateskirt.",
+                    new DigArea(new WorldPoint(3036, 3371, 0), new WorldPoint(3055, 3385, 0)))
+            .put("Dance in the shack in Lumbridge Swamp. Equip a bronze dagger, iron full helmet and a gold ring.",
+                    new DigArea(new WorldPoint(3202, 3167, 0), new WorldPoint(3205, 3170, 0)))
+            .put("Bow outside the entrance to the Legends' Guild. Equip iron platelegs, an emerald amulet and an oak longbow.",
+                    new DigArea(new WorldPoint(2725, 3343, 0), new WorldPoint(2732, 3349, 0)))
+            .put("Bow outside the entrance to the Legends' Guild. Equip iron platelegs, an emerald amulet and an oak longbow.",
+                    new DigArea(new WorldPoint(2725, 3343, 0), new WorldPoint(2732, 3349, 0)))
             .build();
 }
