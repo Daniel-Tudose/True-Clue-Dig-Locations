f763f2863d89dc40f6b652b056dd3bdb22a91bba
Daniel-Tudose <tudosedaniel24@stud.ase.ro>
2026-05-30 08:03:23 +0300

    Add support for 18 more medium emote clue steps

--- src/main/java/io/github/danieltudose/trueclueareas/data/emote/MediumEmoteClueAreas.java
+++ src/main/java/io/github/danieltudose/trueclueareas/data/emote/MediumEmoteClueAreas.java
@@ -12,5 +12,41 @@ public class MediumEmoteClueAreas {
                     new DigArea(new WorldPoint(1668, 3280, 0), new WorldPoint(1676, 3286,0)))
             .put("Beckon in the Shayzien Combat Ring. Show your anger before you talk to me. Equip an adamant platebody, adamant full helm and adamant platelegs.",
                     new DigArea(new WorldPoint(1539, 3619, 0), new WorldPoint(1548, 3627,0)))
+            .put("Dance a jig under Shantay's Awning. Bow before you talk to me. Equip a pointed blue snail helmet, an air staff and a bronze square shield.",
+                    new DigArea(new WorldPoint(3301, 3121, 0), new WorldPoint(3306, 3126,0)))
+            .put("Nod your head where the River Ortus meets the Proudspire. Indicate 'no' before you talk to me. Equip a blue wizard hat, a blue wizard robe and wear nothing on your legs.",
+                    new DigArea(new WorldPoint(1627, 3241, 0), new WorldPoint(1626, 3240,0)))
+            .put("Cheer in the Barbarian Agility Arena. Headbang before you talk to me. Equip a steel platebody, maple shortbow and a Wilderness cape.",
+                    new DigArea(new WorldPoint(2529, 3542, 0), new WorldPoint(2553, 3556,0)))
+            .put("Think in the centre of the Observatory. Spin before you talk to me. Equip a mithril chain body, green dragonhide chaps and a ruby amulet.",
+                    new DigArea(new WorldPoint(2438, 3159, 0), new WorldPoint(2443, 3163,0)))
+            .put("Jump for joy in Yanille bank. Dance a jig before you talk to me. Equip a brown apron, adamantite medium helmet and snakeskin chaps.",
+                    new DigArea(new WorldPoint(2609, 3088, 0), new WorldPoint(2613, 3097,0)))
+            .put("Shrug in Catherby bank. Yawn before you talk to me. Equip a maple longbow, green d'hide chaps and an iron med helm.",
+                    new DigArea(new WorldPoint(2806, 3438, 0), new WorldPoint(2812, 3441,0)))
+            .put("Yawn in the centre of the Arceuus Library. Nod your head before you talk to me. Equip blue dragonhide vambraces, adamant boots and an adamant dagger.",
+                    new DigArea(new WorldPoint(1627, 3802, 0), new WorldPoint(1638, 3813,0)))
+            .put("Spin on the bridge by the Barbarian Village. Salute before you talk to me. Equip purple gloves, a steel kiteshield and a mithril full helmet.",
+                    new DigArea(new WorldPoint(3103, 3420, 0), new WorldPoint(3107, 3421,0)))
+            .put("Cry in the Catherby Ranging shop. Bow before you talk to me. Equip blue gnome boots, a hard leather body and an unblessed silver sickle.",
+                    new DigArea(new WorldPoint(2821, 3441, 0), new WorldPoint(2825, 3445,0)))
+            .put("Beckon in Tai Bwo Wannai. Clap before you talk to me. Equip green dragonhide chaps, a ring of dueling and a mithril medium helmet.",
+                    new DigArea(new WorldPoint(2777, 3059, 0), new WorldPoint(2810, 3073,0)))
+            .put("Beckon in the Digsite, near the eastern winch. Bow before you talk to me. Equip a green gnome hat, snakeskin boots and an iron pickaxe.",
+                    new DigArea(new WorldPoint(3367, 3423, 0), new WorldPoint(3372, 3430,0)))
+            .put("Yawn in the Castle Wars lobby. Shrug before you talk to me. Equip a ruby amulet, a mithril scimitar and a Wilderness cape.",
+                    new DigArea(new WorldPoint(2435, 3081, 0), new WorldPoint(2446, 3098,0)))
+            .put("Dance in the centre of Canifis. Bow before you talk to me. Equip a green gnome robe top, mithril plate legs and an iron two-handed sword.",
+                    new DigArea(new WorldPoint(3485, 3480, 0), new WorldPoint(3503, 3496,0)))
+            .put("Jump for joy in the TzHaar sword shop. Shrug before you talk to me. Equip a Steel longsword, Blue D'hide body and blue mystic gloves.",
+                    new DigArea(new WorldPoint(2476, 5144, 0), new WorldPoint(2479, 5148,0)))
+            .put("Clap in Seers court house. Spin before you talk to me. Equip an adamant halberd, blue mystic robe bottom and a diamond ring.",
+                    new DigArea(new WorldPoint(2732, 3467, 0), new WorldPoint(2739, 3471,0)))
+            .put("Cry in the Draynor Village jail. Jump for joy before you talk to me. Equip an adamant sword, a sapphire amulet and an adamant plateskirt.",
+                    new DigArea(new WorldPoint(3121, 3240, 0), new WorldPoint(3130, 3246,0)))
+            .put("Panic by the mausoleum in Morytania. Wave before you speak to me. Equip a mithril plate skirt, a maple longbow and no boots.",
+                    new DigArea(new WorldPoint(3490, 3567, 0), new WorldPoint(3514, 3581,0)))
+            .put("Cheer in the Edgeville general store. Dance before you talk to me. Equip a brown apron, leather boots and leather gloves.",
+                    new DigArea(new WorldPoint(3076, 3507, 0), new WorldPoint(3084, 3513,0)))
             .build();
 }
