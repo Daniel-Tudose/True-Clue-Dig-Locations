5d038a62d0d36689bef9e68607f42e9f4b70d2ac
Daniel-Tudose <tudosedaniel24@stud.ase.ro>
2026-06-06 18:47:53 +0300

    Add Etceteria's evergreen hard cryptic step (that is off-centered) to the custom map; specified in each cryptic step's comment it's location

--- src/main/java/io/github/danieltudose/trueclueareas/TrueClueAreasPlugin.java
+++ src/main/java/io/github/danieltudose/trueclueareas/TrueClueAreasPlugin.java
@@ -54,14 +54,15 @@ public class TrueClueAreasPlugin extends Plugin {
 		Map<WorldPoint, DigArea> m = new HashMap<>();
 		// Key = base plugin's stored WorldPoint for that clue
 		// Value = the actual dig area with correct SW/NE corners or center tile with width/height
-		m.put(new WorldPoint(2857, 2966, 0),	new DigArea(new WorldPoint(2857, 2965, 0), 7)); //Shilo Village furnace
-		m.put(new WorldPoint(2927, 3761, 0),	new DigArea(new WorldPoint(2927, 3763, 0), 7)); //By the large crossbow from GWD entrance
-		m.put(new WorldPoint(3303, 6092, 0),	new DigArea(new WorldPoint(3303, 6091, 0), 7)); //Priff onion patch
-		m.put(new WorldPoint(2410, 4714, 0),	new DigArea(new WorldPoint(2409, 4715, 0), 7)); //Viyeldi caves
-		m.put(new WorldPoint(3045, 10265, 0),  new DigArea(new WorldPoint(3046, 10265, 0), 7)); //Runite rock in Lava Maze Dungeon
-		m.put(new WorldPoint(2744, 5116, 0),	new DigArea(new WorldPoint(2745, 5115, 0), 7)); //Shadow Dungeon
-		m.put(new WorldPoint(3043, 4974, 1),	new DigArea(new WorldPoint(3043, 4973, 1), 7)); //Fire in the Rogues' Den
-		m.put(new WorldPoint(2874, 3757, 0),	new DigArea(new WorldPoint(2877, 3757, 0), 7)); //North of Trollheim
+		m.put(new WorldPoint(2857, 2966, 0),	new DigArea(new WorldPoint(2857, 2965, 0), 7)); //Shilo Village furnace - Master
+		m.put(new WorldPoint(2927, 3761, 0),	new DigArea(new WorldPoint(2927, 3763, 0), 7)); //By the large crossbow from GWD entrance - Master
+		m.put(new WorldPoint(3303, 6092, 0),	new DigArea(new WorldPoint(3303, 6091, 0), 7)); //Priff onion patch - Master
+		m.put(new WorldPoint(2410, 4714, 0),	new DigArea(new WorldPoint(2409, 4715, 0), 7)); //Viyeldi caves - Master
+		m.put(new WorldPoint(3045, 10265, 0),  new DigArea(new WorldPoint(3046, 10265, 0), 7)); //Runite rock in Lava Maze Dungeon - Master
+		m.put(new WorldPoint(2744, 5116, 0),	new DigArea(new WorldPoint(2745, 5115, 0), 7)); //Shadow Dungeon - Master
+		m.put(new WorldPoint(3043, 4974, 1),	new DigArea(new WorldPoint(3043, 4973, 1), 7)); //Fire in the Rogues' Den - Master
+		m.put(new WorldPoint(2874, 3757, 0),	new DigArea(new WorldPoint(2877, 3757, 0), 7)); //North of Trollheim - Master
+		m.put(new WorldPoint(2591, 3879, 0),	new DigArea(new WorldPoint(2592, 3879, 0), 3)); //Evergreen in Etceteria - Hard
 		CRYPTIC_CUSTOM_AREAS = Collections.unmodifiableMap(m);
 	}
 
