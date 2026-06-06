ef1eb13ec06c9116f911405bf45f3c1aed8210ca
Daniel-Tudose <tudosedaniel24@stud.ase.ro>
2026-06-06 08:51:51 +0300

    Add 11 master emote steps

--- src/main/java/io/github/danieltudose/trueclueareas/data/emote/MasterEmoteClueAreas.java
+++ src/main/java/io/github/danieltudose/trueclueareas/data/emote/MasterEmoteClueAreas.java
@@ -28,6 +28,27 @@ public class MasterEmoteClueAreas {
                     new DigArea(new WorldPoint(2195, 3054, 0), new WorldPoint(2201, 3058, 0)))
             .put("Cheer in the Entrana church. Beware of double agents! Equip a full set of black dragonhide armour.",
                     new DigArea(new WorldPoint(2841, 3345, 0), new WorldPoint(2857, 3351, 0)))
-
+            .put("Show your anger towards the Statue of Saradomin in Ellamaria's garden. Beware of double agents! Equip a zamorak godsword.",
+                    new DigArea(new WorldPoint(3227, 3477, 0), new WorldPoint(3233, 3482, 0)))
+            .put("Dance in Iban's temple. Beware of double agents! Equip Iban's staff, a black mystic top and a black mystic bottom.",
+                    new DigArea(new WorldPoint(2006, 4715, 0), new WorldPoint(2013, 4709, 0)))
+            .put("Stamp in the Enchanted valley west of the waterfall. Beware of double agents! Equip a dragon axe.",
+                    new DigArea(new WorldPoint(3026, 4519, 0), new WorldPoint(3038, 4522, 0)))
+            .put("Spin in front of the Soul altar. Beware of double agents! Equip a dragon pickaxe, helm of neitiznot and a pair of rune boots.",
+                    new DigArea(new WorldPoint(1810, 3850, 0), new WorldPoint(1821, 3589, 0)))
+            .put("Flap at the death altar. Beware of double agents! Equip a death tiara, a legend's cape and any ring of wealth.",
+                    new DigArea(new WorldPoint(2200, 4831, 0), new WorldPoint(2211, 4842, 0)))
+            .put("Swing a bullroarer at the top of the Watchtower. Beware of double agents! Equip a dragon plateskirt, climbing boots and a dragon chainbody.",
+                    new DigArea(new WorldPoint(2927, 4718, 2), new WorldPoint(2934, 4711, 2)))
+            .put("Bow in front of the cave on Brittle Isle. Beware of double agents! Equip a Medallion of the Deep and a rosewood blowpipe.",
+                    new DigArea(new WorldPoint(1945, 4073, 0), new WorldPoint(1947, 4075, 0)))
+            .put("Wave on the northern wall of Castle Drakan. Beware of double agents! Wear a dragon sq shield, splitbark body and any boater.",
+                    new DigArea(new WorldPoint(3546, 3379, 0), new WorldPoint(3562, 3379, 0)))
+            .put("Panic by the big egg where no one dare goes and the ground is burnt. Beware of double agents! Equip a dragon med helm, a TokTz-Ket-Xil, a brine sabre, rune platebody and an uncharged amulet of glory.",
+                    new DigArea(new WorldPoint(3223, 3828, 0), new WorldPoint(3229, 3835, 0)))
+            .put("Slap your head in the centre of the Kourend catacombs. Beware of double agents! Equip arclight or emberlight along with the amulet of the damned.",
+                    new DigArea(new WorldPoint(1659, 10043, 0), new WorldPoint(1668, 10052, 0)))
+            .put("Goblin Salute in the Goblin Village. Beware of double agents! Equip a bandos godsword, a bandos cloak and a bandos platebody.",
+                    new DigArea(new WorldPoint(2949, 3498, 0), new WorldPoint(2964, 3514, 0)))
             .build();
 }
