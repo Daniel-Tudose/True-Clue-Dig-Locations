2ae122204c958326f6f273a36733c26698b6d3ab
Daniel-Tudose <tudosedaniel24@stud.ase.ro>
2026-07-01 18:20:36 +0300

    Fixed hard map step South of Yanille bank center coordinate

--- src/main/java/io/github/danieltudose/trueclueareas/data/map/CustomMapClueAreas.java
+++ src/main/java/io/github/danieltudose/trueclueareas/data/map/CustomMapClueAreas.java
@@ -20,7 +20,7 @@ public class CustomMapClueAreas {
     public static final Map<WorldPoint, DigArea> MAP_STEPS_CUSTOM_AREAS;
     static {
         Map<WorldPoint, DigArea> m = new HashMap<>();
-        m.put(new WorldPoint(2615, 3078, 0),  new DigArea(new WorldPoint(2615, 3077, 0), 3)); // South of Yanille bank - Hard
+        m.put(new WorldPoint(2615, 3078, 0),  new DigArea(new WorldPoint(2616, 3077, 0), 3)); // South of Yanille bank - Hard
         MAP_STEPS_CUSTOM_AREAS = Collections.unmodifiableMap(m);
     }
 }
