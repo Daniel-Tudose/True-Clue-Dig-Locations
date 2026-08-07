315fe068e925532c16e7d2c26dba0bbe9a9a9c52
Daniel-Tudose <tudosedaniel24@stud.ase.ro>
2026-08-07 06:37:25 +0300

    Fix charcoal burners coordinates

--- src/main/java/io/github/danieltudose/trueclueareas/data/emote/EliteEmoteClueAreas.java
+++ src/main/java/io/github/danieltudose/trueclueareas/data/emote/EliteEmoteClueAreas.java
@@ -41,6 +41,6 @@ public class EliteEmoteClueAreas {
             .put("Bow on the ground floor of the Legends' Guild. Equip a Cape of Legends, a dragon battleaxe and an amulet of glory.",
                     new DigArea(new WorldPoint(2723, 3374, 0), new WorldPoint(2735, 3381, 0)))
             .put("Salute by the Charcoal Burners. Equip a Farmer's strawhat, Shayzien platebody (5) and Pyromancer robes.",
-                    new DigArea(new WorldPoint(1644, 3629, 0), new WorldPoint(1648, 3636, 0)))
+                    new DigArea(new WorldPoint(1711, 3464, 0), new WorldPoint(1721, 3468, 0)))
             .build();
 }
