862c730e318ddbbb08294974c7bec7dfc00fc3d8
Daniel Tudose <tudosedaniel24@stud.ase.ro>
2026-08-23 21:28:29 +0300

    Add custom area support for master cryptic Terrorbird display in Varrock Museum step

--- src/main/java/io/github/danieltudose/trueclueareas/data/cryptic/CustomCrypticClueAreas.java
+++ src/main/java/io/github/danieltudose/trueclueareas/data/cryptic/CustomCrypticClueAreas.java
@@ -56,6 +56,7 @@ public class CustomCrypticClueAreas {
         m.put(new WorldPoint(1639, 3673, 0),  new DigArea(new WorldPoint(1630, 3665, 0), new WorldPoint(1643, 3681, 0))); // Center Kourend statue - Easy
         m.put(new WorldPoint(3547, 3183, 0),  new DigArea(new WorldPoint(3535, 3175, 0), new WorldPoint(3572, 3189, 0))); // Fishing spot southeast of Burgh de Rott - Hard
         m.put(new WorldPoint(3081, 3421, 0),  new DigArea(new WorldPoint(3079, 3419, 0), new WorldPoint(3083, 3423, 0))); // Stronghold of Security entrance - Hard
+        m.put(new WorldPoint(1756, 4940, 0),  new DigArea(new WorldPoint(1753, 4936, 0), new WorldPoint(1759, 4942, 0))); // Terrorbird display in Varrock Museum - Master
         CRYPTIC_STEPS_CUSTOM_AREAS = Collections.unmodifiableMap(m);
     }
 }
