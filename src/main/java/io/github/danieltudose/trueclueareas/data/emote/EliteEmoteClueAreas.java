931246410ba2d8380bd0b0aed7a4e387f198d34f
Daniel Tudose <tudosedaniel24@stud.ase.ro>
2026-08-29 00:50:02 +0300

    Reduced the overlay area of the elite emote step headbang at the top of the Slayer Tower to a smaller one

--- src/main/java/io/github/danieltudose/trueclueareas/data/emote/EliteEmoteClueAreas.java
+++ src/main/java/io/github/danieltudose/trueclueareas/data/emote/EliteEmoteClueAreas.java
@@ -11,7 +11,7 @@ public class EliteEmoteClueAreas {
             .put("Jump for joy in the Ancient Cavern. Equip a granite shield, splitbark body and any rune heraldic helm.",
                     new DigArea(new WorldPoint(1763, 5365, 1), new WorldPoint(1768, 5367, 1)))
             .put("Headbang at the top of Slayer Tower. Equip a seercull, a combat bracelet and helm of Neitiznot.",
-                    new DigArea(new WorldPoint(3405, 3531, 2), new WorldPoint(3452, 3579, 2)))
+                    new DigArea(new WorldPoint(3416, 3534, 2), new WorldPoint(3425, 3543, 2)))
             .put("Spin in West Ardougne Church. Equip a dragon spear and red dragonhide chaps.",
                     new DigArea(new WorldPoint(2524, 3285, 0), new WorldPoint(2532, 3295, 0)))
             .put("Blow a kiss in the heart of the lava maze. Equip black dragonhide chaps, a spotted cape and a rolling pin.",
